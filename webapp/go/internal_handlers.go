64670bb28bad748fd32a291249c4ea8dd60b27c3
furusax0621 <fur.sax.smallworld@gmail.com>
2024-12-28 00:06:49 +0900

    ライドと椅子のマッチングアルゴリズムを改善

--- webapp/go/internal_handlers.go
+++ webapp/go/internal_handlers.go
@@ -1,67 +1,76 @@
 package main
 
 import (
-	"database/sql"
-	"errors"
+	"math"
 	"net/http"
+	"slices"
 )
 
 // このAPIをインスタンス内から一定間隔で叩かせることで、椅子とライドをマッチングさせる
 func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	// MEMO: 一旦最も待たせているリクエストに適当な空いている椅子マッチさせる実装とする。おそらくもっといい方法があるはず…
-	ride := &Ride{}
-	if err := db.GetContext(ctx, ride, `SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at LIMIT 1`); err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			w.WriteHeader(http.StatusNoContent)
-			return
-		}
+
+	// まだ椅子が割り当てられていないライドを取得。ループの都合上、より時間が経過しているものから取得
+	rides := []Ride{}
+	if err := db.SelectContext(ctx, &rides, `SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at ASC`); err != nil {
 		writeError(w, http.StatusInternalServerError, err)
 		return
 	}
 
-	matched := &Chair{}
-	empty := false
-	for i := 0; i < 10; i++ {
-		if err := db.GetContext(ctx, matched, "SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1"); err != nil {
-			if errors.Is(err, sql.ErrNoRows) {
-				w.WriteHeader(http.StatusNoContent)
-				return
-			}
-			writeError(w, http.StatusInternalServerError, err)
-		}
-
-		if err := db.GetContext(ctx, &empty, "SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE", matched.ID); err != nil {
-			writeError(w, http.StatusInternalServerError, err)
-			return
-		}
-		if empty {
-			break
-		}
-	}
-	if !empty {
+	// 割当待ちのライドがなければ一件落着
+	if len(rides) == 0 {
 		w.WriteHeader(http.StatusNoContent)
 		return
 	}
 
-	tx, err := db.Beginx()
-	if err != nil {
+	// 現在空いている椅子を取得
+	chairs := []ChairWithLocation{}
+	if err := db.SelectContext(ctx, &chairs, `SELECT * FROM chairs WHERE is_free = TRUE AND is_active = TRUE`); err != nil {
 		writeError(w, http.StatusInternalServerError, err)
 		return
 	}
-	defer tx.Rollback()
 
-	if _, err := tx.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", matched.ID, ride.ID); err != nil {
-		writeError(w, http.StatusInternalServerError, err)
-		return
-	}
-	if _, err := tx.ExecContext(ctx, "UPDATE chairs SET is_free = FALSE WHERE id = ?", matched.ID); err != nil {
-		writeError(w, http.StatusInternalServerError, err)
-		return
-	}
-	if err := tx.Commit(); err != nil {
-		writeError(w, http.StatusInternalServerError, err)
-		return
+	// ライドと椅子をマッチング
+	for _, ride := range rides {
+		distance := math.MaxInt
+		var matched ChairWithLocation
+		var matchedIndex int
+		for i, chair := range chairs {
+			d := calculateDistance(ride.PickupLatitude, ride.PickupLongitude, chair.Latitude, chair.Longitude)
+			if d < distance {
+				distance = d
+				matched = chair
+				matchedIndex = i
+			}
+		}
+
+		// マッチングした椅子をライドに割り当て
+		err := func() error {
+			tx, err := db.Beginx()
+			if err != nil {
+				return err
+			}
+			defer tx.Rollback()
+
+			if _, err := tx.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", matched.ID, ride.ID); err != nil {
+				return err
+			}
+			if _, err := tx.ExecContext(ctx, "UPDATE chairs SET is_free = FALSE WHERE id = ?", matched.ID); err != nil {
+				return err
+			}
+			if err := tx.Commit(); err != nil {
+				return err
+			}
+
+			return nil
+		}()
+		if err != nil {
+			writeError(w, http.StatusInternalServerError, err)
+			return
+		}
+
+		// 割り当てた椅子を割り当て待ちから削除
+		chairs = slices.Delete(chairs, matchedIndex, 1)
 	}
 
 	w.WriteHeader(http.StatusNoContent)
